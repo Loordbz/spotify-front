b093ae5ed10c3d32dc3d77c78aa5f64e59175a55
Leonardo Berto <l300000000@gmail.com>
2025-05-14 22:54:22 -0300

    Primeiros códigos na home e criando mock de dados na service

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MediaService } from '../../services/media.service';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-home',
-  imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrls: ['./home.component.scss'],
+  imports: [
+    CommonModule,
+  ]
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  mediaList: any[] = [];
 
+  constructor(private mediaService: MediaService) {}
+
+  ngOnInit(): void {
+    this.mediaService.getMedia().subscribe((data: any) => {
+      this.mediaList = data;
+    });
+  }
 }
--- src/app/services/media.service.ts
+++ src/app/services/media.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import { of } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MediaService {
-
-  constructor() { }
+  getMedia() {
+    return of([
+      {
+        id: 1,
+        title: 'Top Hits 2025',
+        thumbnail: '',
+      },
+      {
+        id: 2,
+        title: 'Rock Classics',
+        thumbnail: '',
+      },
+      {
+        id: 3,
+        title: 'Jazz Vibes',
+        thumbnail: '',
+      },
+    ]);
+  }
 }
+
