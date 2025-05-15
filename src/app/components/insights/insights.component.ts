6c53dcea0d1c2374c6216383ab12da3c540af17c
Leonardo Berto <l300000000@gmail.com>
2025-05-14 22:54:45 -0300

    Criando um gráfico pizza com dados mocados

--- src/app/components/insights/insights.component.ts
+++ src/app/components/insights/insights.component.ts
@@ -1,11 +1,69 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
+import {
+  Chart,
+  ArcElement,
+  Tooltip,
+  Legend,
+  Title,
+  PieController
+} from 'chart.js';
+
+// Registrar os elementos necessários para o gráfico de pizza
+Chart.register(ArcElement, Tooltip, Legend, Title, PieController);
 
 @Component({
   selector: 'app-insights',
-  imports: [],
   templateUrl: './insights.component.html',
-  styleUrl: './insights.component.scss'
+  styleUrls: ['./insights.component.scss']
 })
-export class InsightsComponent {
+export class InsightsComponent implements AfterViewInit {
+  @ViewChild('pieChart') pieChartRef!: ElementRef;
 
+  ngAfterViewInit() {
+    this.createPieChart();
+  }
+
+  createPieChart(): any {
+    if (this.pieChartRef) {
+      const canvas = this.pieChartRef.nativeElement;
+      
+      const chart = new Chart(canvas, {
+        type: 'pie',
+        data: {
+          labels: ['Rock Classics', 'Jazz Vibes', 'Top Hits', 'Chill Beats', 'Focus'],
+          datasets: [{
+            label: 'Reproduções',
+            data: [120, 90, 75, 60, 45],
+            backgroundColor: [
+              '#1db954',
+              '#ff6384',
+              '#36a2eb',
+              '#ffcd56',
+              '#9966ff'
+            ],
+            borderColor: '#111',
+            borderWidth: 1
+          }]
+        },
+        options: {
+          responsive: true,
+          plugins: {
+            title: {
+              display: true,
+              text: 'Distribuição de Reproduções por Categoria',
+              color: '#fff',
+              font: { size: 18 }
+            },
+            legend: {
+              labels: {
+                color: '#fff'
+              }
+            }
+          }
+        }
+      });
+
+      return chart;
+    }
+  }
 }
