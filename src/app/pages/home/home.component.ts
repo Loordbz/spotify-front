cb201573f47444c2c9007c8efd9fcc8ae2623fd4
Leonardo Berto <l300000000@gmail.com>
2025-05-16 10:16:35 -0300

    alteração da home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,7 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-import { MediaService } from '../../services/media.service';
+import { MediaService } from '../../core/services/media.service';
 import { CommonModule } from '@angular/common';
+import { MenuLeftComponent } from '../../components/menu-left/menu-left.component';
+import { FormsModule } from '@angular/forms';
+import { CardArtistComponent } from '../../components/card-artist/card-artist.component';
+import { CardAlbumComponent } from '../../components/card-album/card-album.component';
 
+export interface Artist {
+  name: string;
+  url: string;
+}
+
+export interface Album {
+  name: string;
+  url: string;
+  artist: string;
+}
 
 @Component({
   selector: 'app-home',
@@ -9,16 +23,34 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./home.component.scss'],
   imports: [
     CommonModule,
+    FormsModule,
+    MenuLeftComponent,
+    CardArtistComponent,
+    CardAlbumComponent
   ]
 })
 export class HomeComponent implements OnInit {
-  mediaList: any[] = [];
+  artists: Artist[] = [];
+  albums: Album[] = []; 
 
-  constructor(private mediaService: MediaService) {}
+  constructor(private mediaService: MediaService) { }
 
   ngOnInit(): void {
-    this.mediaService.getMedia().subscribe((data: any) => {
-      this.mediaList = data;
+    this.mediaService.getPersonData().subscribe((data: any) => {
+      data.forEach((item: any) => {
+        if (item.artist) {
+          this.albums.push({
+            name: item.name,  
+            url: item.url,     
+            artist: item.artist 
+          });
+        } else {
+          this.artists.push({
+            name: item.name,   
+            url: item.url 
+          });
+        }
+      });
     });
   }
 }
