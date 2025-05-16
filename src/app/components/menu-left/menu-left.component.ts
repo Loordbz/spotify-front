d3e5ff6fb7f2ca32d5cf3607896d13af4768be2f
Leonardo Berto <l300000000@gmail.com>
2025-05-15 23:09:45 -0300

    Criação do menu lateral

---
+++ src/app/components/menu-left/menu-left.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+@Component({
+  selector: 'app-menu-left',
+  templateUrl: './menu-left.component.html',
+  styleUrls: ['./menu-left.component.scss'],
+  imports: [
+    CommonModule,
+  ]
+})
+export class MenuLeftComponent {
+
+}
